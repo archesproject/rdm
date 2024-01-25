0fa7faf9c5d62276a5d7624d171caa5a89c3f5db
Johnathan Clementi <johnathan.clementi1@gmail.com>
2024-01-25 18:06:53 -0500

    Update urls.py

--- arches_rdm/urls.py
+++ arches_rdm/urls.py
@@ -13,7 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.urls import include, path
 
-
-urlpatterns = []
+urlpatterns = [
+    path('', include('arches.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
