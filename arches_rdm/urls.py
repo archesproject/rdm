bd95c4fd526027afbad98918824bf8812cd11718
Johnathan Clementi <johnathan.clementi1@gmail.com>
2024-01-26 09:02:51 -0500

    Undo changes that caused malformed requests

--- arches_rdm/urls.py
+++ arches_rdm/urls.py
@@ -13,11 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
-from django.urls import include, path
+from django.urls import path
 
-urlpatterns = [
-    path('', include('arches.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = []
