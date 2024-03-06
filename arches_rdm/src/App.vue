b62c899b84f7f49f7136eaf3cc4adcb35743272c
Jacob Walls <jwalls@fargeo.com>
2024-03-06 18:25:42 -0500

    Add typescript and stub out login

--- arches_rdm/src/App.vue
+++ arches_rdm/src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
-import Foo from "@/components/Foo.vue"
+<script setup lang="ts">
+import { ref } from "vue";
+
+import HomePage from "@/components/HomePage.vue";
+import LoginPage from "@/components/LoginPage.vue";
+
+const isAuthenticated = ref(false);
 </script>
 
 <template>
-    <Foo />
+    <component
+        :is="isAuthenticated ? HomePage : LoginPage"
+        :v-model:is-authenticated="isAuthenticated"
+    />
 </template>
--- arches_rdm/src/components/Foo.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-console.log('hello world!')
-</script>
-
-<template>
-    <h1>Hello World!</h1>
-</template>
