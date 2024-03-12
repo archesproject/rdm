5090c291e4f1cea6c7427e93ba234a0eb31ae130
Jacob Walls <jwalls@fargeo.com>
2024-03-12 09:44:09 -0400

    Add expand, collapse, language buttons

--- arches_rdm/src/components/ConceptTree.vue
+++ arches_rdm/src/components/ConceptTree.vue
@@ -4,6 +4,8 @@ import { computed, ref } from "vue";
 import { useGettext } from "vue3-gettext";
 
 import { useToast } from "primevue/usetoast";
+import Button from "primevue/button";
+import Dropdown from "primevue/dropdown";
 import Tree from "primevue/tree";
 
 import { bestLabel } from "@/utils";
@@ -17,10 +19,22 @@ import type {
 } from "primevue/tree/Tree";
 import type { Concept, Scheme } from "@/types";
 
+// todo(jtw): get from server when implementing other languages
+const ENGLISH = {
+    code: "en",
+    default_direction: "ltr",
+    id: 1,
+    isdefault: true,
+    name: 'English',
+    scope: 'system',
+};
+const LANGUAGE_CHOICES = [ENGLISH];
+
 const expandedKeys: Ref<TreeExpandedKeys> = ref({});
 const selectionKeys: Ref<TreeSelectionKeys> = ref({});
 const filterValue = ref("");
 const schemes: Ref<Scheme[]> = ref([]);
+const selectedLanguage: Ref<Language> = ref(ENGLISH);
 
 const toast = useToast();
 const { $gettext } = useGettext();
@@ -80,6 +94,26 @@ const conceptTree = computed(() => {
     return schemes.value.map((scheme: Scheme) => schemeAsNode(scheme));
 });
 
+const expandAll = () => {
+    for (const node of conceptTree.value) {
+        expandNode(node);
+    }
+    expandedKeys.value = { ...expandedKeys.value };
+};
+
+const collapseAll = () => {
+    expandedKeys.value = {};
+};
+
+const expandNode = (node: TreeNode) => {
+    if (node.children && node.children.length) {
+        expandedKeys.value[node.key] = true;
+        for (const child of node.children) {
+            expandNode(child);
+        }
+    }
+};
+
 const fetchSchemes = async () => {
     let errorText;
     const url = new URL("concept_trees/", DJANGO_HOST);
@@ -107,6 +141,43 @@ await fetchSchemes();
 </script>
 
 <template>
+    <div class="controls">
+        <Button
+            class="control"
+            type="button"
+            icon="fa fa-plus"
+            :label="$gettext('Expand')"
+            @click="expandAll"
+            :pt="{
+                icon: { style: { color: 'black', fontSize: 'x-small' } },
+                label: { style: { color: 'black', fontWeight: '200' } },
+            }"
+        />
+        <Button
+            class="control"
+            type="button"
+            icon="fa fa-minus"
+            :label="$gettext('Collapse')"
+            @click="collapseAll"
+            :pt="{
+                icon: { style: { color: 'black', fontSize: 'x-small' } },
+                label: { style: { color: 'black', fontWeight: '200' } },
+            }"
+        />
+        <Dropdown
+            v-model="selectedLanguage"
+            :options="LANGUAGE_CHOICES"
+            option-label="name"
+            :placeholder="$gettext('Language')"
+            checkmark
+            :highlight-on-select="false"
+            :pt="{
+                root: { class: 'control' },
+                input: { style: { fontFamily: 'inherit', fontSize: 'small', textAlign: 'center', fontWeight: '200' } },
+                itemLabel: { style: { fontSize: 'small' } },
+            }"
+        />
+    </div>
     <Tree
         v-model:selectionKeys="selectionKeys"
         :value="conceptTree"
@@ -139,3 +210,28 @@ await fetchSchemes();
         </template>
     </Tree>
 </template>
+
+<style scoped>
+.controls {
+    display: flex;
+    background: #f3fbfd;
+    padding: 1rem;
+    gap: 0.5rem;
+}
+
+.control {
+    flex: 0.33;
+    border: 0;
+    background: lightgray;
+    font-size: smaller;
+}
+
+.button {
+    font-size: small;
+    height: 4rem;
+    margin: 0.5rem;
+    justify-content: center;
+    font-weight: 200;
+    text-wrap: nowrap;
+}
+</style>
