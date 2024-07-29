a7b492cb9651df77fb815b9aad81575d3df3bbf2
Johnathan Clementi <johnathan.clementi1@gmail.com>
2024-07-29 17:15:39 -0400

    nit

--- arches_lingo/apps.py
+++ arches_lingo/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class Arches_LingoConfig(AppConfig):
     name = "arches_lingo"
-    verbose_name = "Arches_Lingo"
+    verbose_name = "Arches Lingo"
     is_arches_application = True
