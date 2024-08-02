ccac31f8259c6f4b7853bf9c6eaa37ffaa2d4c7b
Jacob Walls <jwalls@fargeo.com>
2024-08-02 14:57:45 -0400

    Remove underscore

--- arches_lingo/apps.py
+++ arches_lingo/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 from arches.settings_utils import generate_frontend_configuration
 
 
-class Arches_LingoConfig(AppConfig):
+class ArchesLingoConfig(AppConfig):
     name = "arches_lingo"
     verbose_name = "Arches Lingo"
     is_arches_application = True
