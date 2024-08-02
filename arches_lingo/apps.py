04054b17f533f7b9a69b46d6770250c55d708e1f
Jacob Walls <jwalls@fargeo.com>
2024-08-02 14:57:06 -0400

    Reconcile apps.py and github actions

--- arches_lingo/apps.py
+++ arches_lingo/apps.py
@@ -1,7 +1,12 @@
 from django.apps import AppConfig
 
+from arches.settings_utils import generate_frontend_configuration
+
 
 class Arches_LingoConfig(AppConfig):
     name = "arches_lingo"
     verbose_name = "Arches Lingo"
     is_arches_application = True
+
+    def ready(self):
+        generate_frontend_configuration()
