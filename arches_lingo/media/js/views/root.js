a6114f78de7ed6bf342891d1cda7d89ad7a359ab
Christopher Byrd <cbyrd@fargeo.com>
2024-08-12 10:42:41 -0700

    fixes frontend test coverage #48 (#49)

--- arches_lingo/media/js/views/root.js
+++
@@ -1,6 +0,0 @@
-import LingoApp from '@/arches_lingo/App.vue';
-import createVueApplication from 'arches/arches/app/media/js/utils/create-vue-application';
-
-createVueApplication(LingoApp).then(vueApp => {
-    vueApp.mount('#lingo-mounting-point');
-});
