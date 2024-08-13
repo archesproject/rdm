57beb57dd423f7edb55261a35f3f2874e917b011
Jacob Walls <jwalls@fargeo.com>
2024-08-13 16:58:27 -0400

    Add lazy route loading

--- arches_lingo/src/arches_lingo/constants.ts
+++ arches_lingo/src/arches_lingo/constants.ts
@@ -1,11 +1,6 @@
 import type { InjectionKey } from "vue";
 
 import type { UserRefAndSetter } from "@/arches_lingo/types";
-import HomePage from "@/arches_lingo/pages/home/HomePage.vue";
-import LoginPage from "@/arches_lingo/pages/login/LoginPage.vue";
-import BasicSearch from "@/arches_lingo/pages/search-basic/BasicSearch.vue";
-import AdvancedSearch from "@/arches_lingo/pages/search-advanced/AdvancedSearch.vue";
-import SchemeList from "@/arches_lingo/pages/scheme-list/SchemeList.vue";
 
 export const ERROR = "error";
 export const DEFAULT_ERROR_TOAST_LIFE = 8000;
@@ -13,15 +8,34 @@ export const DEFAULT_ERROR_TOAST_LIFE = 8000;
 export const userKey = Symbol() as InjectionKey<UserRefAndSetter>;
 
 export const routes = [
-    { path: "/", name: "root", component: HomePage },
-    { path: "/login/:next?", name: "login", component: LoginPage },
-    { path: "/search", name: "search", component: BasicSearch },
+    {
+        path: "/",
+        name: "root",
+        component: () => import("@/arches_lingo/pages/home/HomePage.vue"),
+    },
+    {
+        path: "/login/:next?",
+        name: "login",
+        component: () => import("@/arches_lingo/pages/login/LoginPage.vue"),
+    },
+    {
+        path: "/search",
+        name: "search",
+        component: () =>
+            import("@/arches_lingo/pages/search-basic/BasicSearch.vue"),
+    },
     {
         path: "/advanced-search",
         name: "advanced-search",
-        component: AdvancedSearch,
+        component: () =>
+            import("@/arches_lingo/pages/search-advanced/AdvancedSearch.vue"),
     },
-    { path: "/schemes", name: "schemes", component: SchemeList },
+    {
+        path: "/schemes",
+        name: "schemes",
+        component: () =>
+            import("@/arches_lingo/pages/scheme-list/SchemeList.vue"),
+    },
 ];
 
 export const routeNames = {
