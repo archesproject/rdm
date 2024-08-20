e54f706e65cfef4fe6a84fac88449d09382f434f
Jacob Walls <jwalls@fargeo.com>
2024-08-20 17:05:25 -0400

    Small view cleanups

--- arches_lingo/views/root.py
+++ arches_lingo/views/root.py
@@ -8,7 +8,7 @@ from arches.app.views.base import BaseManagerView
 
 class LingoRootView(BaseManagerView):
     @method_decorator(ensure_csrf_cookie)
-    def get(self, request, graphid=None, resourceid=None):
+    def get(self, request, *args, **kwargs):
         context = self.get_context_data(main_script="views/root")
         context["page_title"] = _("Lingo")
         return render(request, "arches_lingo/root.htm", context)
