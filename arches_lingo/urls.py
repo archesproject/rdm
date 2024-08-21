c05e7aaf28fa08e91041467cb4f4d24a4913e470
Jacob Walls <jwalls@fargeo.com>
2024-08-21 18:57:13 -0400

    Initial commit of search backend #67

--- arches_lingo/urls.py
+++ arches_lingo/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 
-from arches_lingo.views import LingoRootView, ConceptTreeView
+from arches_lingo.views import LingoRootView, ConceptTreeView, ValueSearchView
 
 urlpatterns = [
     path("", LingoRootView.as_view(), name="root"),
@@ -12,6 +12,7 @@ urlpatterns = [
     path("advanced-search", LingoRootView.as_view(), name="advanced-search"),
     path("schemes", LingoRootView.as_view(), name="schemes"),
     path("api/concept_trees", ConceptTreeView.as_view(), name="concept_trees"),
+    path("api/search", ValueSearchView.as_view(), name="api_search"),
     path("", include("arches_references.urls")),
 ]
 
