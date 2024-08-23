818b51fdc7a5a45e917c65f97f5b0f716a3ad8a4
Christopher Byrd <chrabyrd@gmail.com>
2024-08-23 16:15:22 -0700

    rough out search bar #19

--- arches_lingo/src/arches_lingo/pages/BasicSearch.vue
+++ arches_lingo/src/arches_lingo/pages/BasicSearch.vue
@@ -1,1 +1,10 @@
-<template>Basic Search placeholder</template>
+<script setup lang="ts">
+import BasicSearchComponent from '@/arches_lingo/components/basic-search/BasicSearchComponent.vue';
+
+</script>
+
+<template>
+    <div style="height: 100%; padding: 6rem;">
+        <BasicSearchComponent />
+    </div>
+</template>
