2c94a9511b8595f47cf33ef87b6737b0b3de5ea9
Christopher Byrd <chrabyrd@gmail.com>
2024-08-26 20:03:14 -0700

    nit #19

---
+++ arches_lingo/src/arches_lingo/components/basic-search/SearchResult.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  foo: {
+    type: Object,
+    required: true
+  },
+})
+
+</script>
+
+<template>
+    <div 
+        :class="{ 
+            'is-even': foo.index % 2 === 0,
+        }"
+        style="width: 100%; padding: 1rem;"
+    >
+        <!-- {{ console.log(foo, this) }} -->
+        <div>{{ foo.option.labels.find(bar => bar.language === 'en')['value'] }}</div>
+    </div>
+</template>
+
+<style scoped>
+.is-even {
+    background-color: lightblue;
+}
+</style>
