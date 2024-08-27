1709d4981fe885008375657eb612349f0fba71e2
Christopher Byrd <chrabyrd@gmail.com>
2024-08-26 23:03:03 -0700

    nit #19

--- arches_lingo/src/arches_lingo/components/basic-search/SortAndFilterControls.vue
+++ arches_lingo/src/arches_lingo/components/basic-search/SortAndFilterControls.vue
@@ -16,6 +16,7 @@ const querySortPreference = ref();
             margin-top: 0.2rem;
             display: flex;
             justify-content: space-between;
+            align-items: center;
         "
     >
         <div style="display: flex;">
@@ -24,15 +25,15 @@ const querySortPreference = ref();
             </div>
 
             <div style="display: flex;">
-                <div>
+                <div class="query-sort-preference">
                     <RadioButton v-model="querySortPreference" inputId="querySortPreference1" name="querySortPreference" value="Unsorted" />
                     <label for="querySortPreference1">Unsorted</label>
                 </div>
-                <div>
+                <div class="query-sort-preference">
                     <RadioButton v-model="querySortPreference" inputId="querySortPreference2" name="querySortPreference" value="A to Z" />
                     <label for="querySortPreference2">A to Z</label>
                 </div>
-                <div>
+                <div class="query-sort-preference">
                     <RadioButton v-model="querySortPreference" inputId="querySortPreference3" name="querySortPreference" value="Z to A" />
                     <label for="querySortPreference3">Z to A</label>
                 </div>
@@ -44,23 +45,23 @@ const querySortPreference = ref();
             </div>
     
             <div style="display: flex;">
-                <div>
+                <div class="query-filter">
                     <Checkbox v-model="queryFilters" inputId="queryFilter1" name="queryFilters" value="Concepts" />
                     <label for="queryFilter1"> Concepts </label>
                 </div>
-                <div>
+                <div class="query-filter">
                     <Checkbox v-model="queryFilters" inputId="queryFilter2" name="queryFilters" value="Places" />
                     <label for="queryFilter2"> Places </label>
                 </div>
-                <div>
+                <div class="query-filter">
                     <Checkbox v-model="queryFilters" inputId="queryFilter3" name="queryFilters" value="People" />
                     <label for="queryFilter3"> People </label>
                 </div>
-                <div>
+                <div class="query-filter">
                     <Checkbox v-model="queryFilters" inputId="queryFilter4" name="queryFilters" value="Groups" />
                     <label for="queryFilter4"> Groups </label>
                 </div>
-                <div>
+                <div class="query-filter">
                     <Checkbox v-model="queryFilters" inputId="queryFilter4" name="queryFilters" value="Periods" />
                     <label for="queryFilter4"> Periods </label>
                 </div>
@@ -68,3 +69,17 @@ const querySortPreference = ref();
         </div>
     </div>
 </template>
+
+<style scoped>
+.query-sort-preference,
+.query-filter {
+    margin: 0 0.5rem;
+}
+
+.query-sort-preference > :nth-child(2),
+.query-filter > :nth-child(2) {
+    margin: 0 0.25rem;
+    cursor: pointer;
+}
+
+</style>
