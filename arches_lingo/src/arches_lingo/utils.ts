0f31bb6a0a8b6c582e2fab3302481ac8d95419b1
Jacob Walls <jwalls@fargeo.com>
2024-08-29 10:41:13 -0400

    Add <LetterCircle>

--- arches_lingo/src/arches_lingo/utils.ts
+++ arches_lingo/src/arches_lingo/utils.ts
@@ -1,4 +1,5 @@
-import type { Labellable } from "@/arches_lingo/types";
+import type { TreeNode } from "primevue/treenode";
+import type { Labellable, Scheme } from "@/arches_lingo/types";
 
 export const bestLabel = (item: Labellable, languageCode: string) => {
     const labelsInLang = item.labels.filter(
@@ -12,4 +13,18 @@ export const bestLabel = (item: Labellable, languageCode: string) => {
         return item.labels[0];
     }
     return bestLabel;
+};
+
+// Duck-typing helpers
+export const dataIsScheme = (data: Labellable) => {
+    return (data as Scheme).top_concepts !== undefined;
+};
+export const dataIsConcept = (data: Labellable) => {
+    return !dataIsScheme(data);
+};
+export const nodeIsConcept = (node: TreeNode) => {
+    return !nodeIsScheme(node);
+};
+export const nodeIsScheme = (node: TreeNode) => {
+    return dataIsScheme(node.data);
 };
