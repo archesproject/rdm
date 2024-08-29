68b7100e8639f572b4143d1b2b9520a0cc246a23
Jacob Walls <jwalls@fargeo.com>
2024-08-29 12:03:10 -0400

    Factor out tree builder util

--- arches_lingo/src/arches_lingo/utils.ts
+++ arches_lingo/src/arches_lingo/utils.ts
@@ -1,4 +1,12 @@
-import type { Concept, Labellable, Scheme } from "@/arches_lingo/types";
+import type { TreeNode } from "primevue/treenode";
+import type { Language } from "@/arches/types";
+import type {
+    Concept,
+    IconLabels,
+    Labellable,
+    NodeAndParentInstruction,
+    Scheme,
+} from "@/arches_lingo/types";
 
 export const getItemLabel = (
     item: Concept,
@@ -56,4 +64,80 @@ export const nodeIsConcept = (node: TreeNode) => {
 };
 export const nodeIsScheme = (node: TreeNode) => {
     return dataIsScheme(node.data);
+};
+
+// Tree builder
+export const treeFromSchemes = (
+    schemes: Scheme[],
+    selectedLanguage: Language,
+    iconLabels: IconLabels,
+    focusedNode: TreeNode | null,
+) => {
+    // Use a closure to avoid passing around params (selectedLanguage, etc).
+    const conceptAsNodeAndInstruction = (
+        concept: Concept,
+    ): NodeAndParentInstruction => {
+        return conceptOrSchemeAsNodeAndInstruction(concept, concept.narrower);
+    };
+    const schemeAsNodeAndInstruction = (
+        scheme: Scheme,
+    ): NodeAndParentInstruction => {
+        return conceptOrSchemeAsNodeAndInstruction(scheme, scheme.top_concepts);
+    };
+
+    const conceptOrSchemeAsNodeAndInstruction = (
+        labellable: Labellable,
+        children: Concept[],
+    ): NodeAndParentInstruction => {
+        let childrenAsNodes: TreeNode[];
+        const nodesAndInstructions = children.map((child) =>
+            conceptAsNodeAndInstruction(child),
+        );
+        const parentOfFocusedNode = nodesAndInstructions.find(
+            (obj) => obj.parentShouldHideSiblings,
+        );
+        if (parentOfFocusedNode) {
+            childrenAsNodes = [parentOfFocusedNode.node];
+        } else {
+            childrenAsNodes = nodesAndInstructions.map((obj) => obj.node);
+        }
+
+        const node: TreeNode = {
+            key: labellable.id,
+            label: bestLabel(labellable, selectedLanguage.code).value,
+            children: childrenAsNodes,
+            data: labellable,
+            iconLabel: dataIsScheme(labellable)
+                ? iconLabels.scheme
+                : iconLabels.concept,
+        };
+        let parentShouldHideSiblings = !!parentOfFocusedNode;
+        if (!parentShouldHideSiblings) {
+            const focalNodeIdx = node.children!.findIndex(
+                (child: TreeNode) => child.data.id === focusedNode?.data?.id,
+            );
+            if (focalNodeIdx > -1) {
+                node.children = [node.children![focalNodeIdx]];
+                parentShouldHideSiblings = true;
+            }
+        }
+        return { node, parentShouldHideSiblings };
+    };
+
+    const focalNodeIdx = schemes.findIndex(
+        (scheme) => scheme.id === focusedNode?.data?.id,
+    );
+    if (focalNodeIdx > -1) {
+        return [schemeAsNodeAndInstruction(schemes[focalNodeIdx]).node];
+    }
+    const nodesAndInstructions = schemes.map((scheme: Scheme) =>
+        schemeAsNodeAndInstruction(scheme),
+    );
+    const parentOfFocusedNode = nodesAndInstructions.find(
+        (obj) => obj.parentShouldHideSiblings,
+    );
+    if (parentOfFocusedNode) {
+        return [parentOfFocusedNode.node];
+    }
+    return nodesAndInstructions.map((obj) => obj.node);
 };
