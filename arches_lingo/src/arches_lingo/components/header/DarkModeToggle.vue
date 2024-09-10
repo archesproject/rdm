bb28a8f1b5a926f7da4cb1ce50608cac8b065069
Jacob Walls <jwalls@fargeo.com>
2024-09-10 16:22:10 -0400

    Add dark mode toggle

---
+++ arches_lingo/src/arches_lingo/components/header/DarkModeToggle.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useGettext } from "vue3-gettext";
+
+import ToggleButton from "primevue/togglebutton";
+
+import { DEFAULT_THEME } from "@/arches/themes/default.ts";
+
+const { $gettext } = useGettext();
+
+const isDarkModeEnabled = ref(
+    window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches,
+);
+
+const toggleDarkMode = () => {
+    const element = document.querySelector("html");
+    // Remove the leading dot from the selector to get class name.
+    element!.classList.toggle(
+        DEFAULT_THEME.theme.options.darkModeSelector.substring(1),
+    );
+    isDarkModeEnabled.value = !isDarkModeEnabled.value;
+};
+</script>
+
+<template>
+    <ToggleButton
+        :model-value="isDarkModeEnabled"
+        :on-label="$gettext('Dark')"
+        :off-label="$gettext('Light')"
+        off-icon="pi pi-sun"
+        on-icon="pi pi-moon"
+        :aria-label="$gettext('Toggle dark mode')"
+        :pt="{ label: { style: { display: 'none' } } }"
+        @click="toggleDarkMode"
+    />
+</template>
