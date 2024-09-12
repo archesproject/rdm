52496057f411c074bf0b26e41f0d112eba5ab07e
Jacob Walls <jwalls@fargeo.com>
2024-09-12 16:56:50 -0400

    Add scheme route and detail pages #15

---
+++ arches_lingo/src/arches_lingo/components/detail/ConceptDetail.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { Concept } from "@/arches_lingo/types";
+
+const { concept } = defineProps<{ concept: Concept }>();
+</script>
+
+<template>
+    {{ concept }}
+</template>
