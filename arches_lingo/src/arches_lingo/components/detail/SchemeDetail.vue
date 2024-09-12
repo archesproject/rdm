9636ca1bc916aa8682608a150da26e13ed3f9f29
Jacob Walls <jwalls@fargeo.com>
2024-09-12 16:56:50 -0400

    Add scheme route and detail pages #15

---
+++ arches_lingo/src/arches_lingo/components/detail/SchemeDetail.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { Scheme } from "@/arches_lingo/types";
+
+const { scheme } = defineProps<{ scheme: Scheme }>();
+</script>
+
+<template>
+    {{ scheme }}
+</template>
