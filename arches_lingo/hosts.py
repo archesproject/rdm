857829796e5999dc6aa10f18be742a694a94ead0
Aaron Gundel <aaron.gundel@gmail.com>
2024-10-24 15:15:44 -0600

    update hosts, run deploy again

--- arches_lingo/hosts.py
+++ arches_lingo/hosts.py
@@ -2,6 +2,7 @@ import re
 from django_hosts import patterns, host
 
 host_patterns = patterns(
-    "",
     host(re.sub(r"_", r"-", r"arches_lingo"), "arches_lingo.urls", name="arches_lingo"),
+    host(r"arches", "arches.urls", name="arches"),
+    "",
 )
