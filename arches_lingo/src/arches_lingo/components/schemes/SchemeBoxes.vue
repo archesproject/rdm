4d369be024ff102572022be7b35f994f5b665c6c
Johnathan Clementi <johnathan.clementi1@gmail.com>
2024-11-06 09:51:16 -0500

    Stub out schemes list page #16

---
+++ arches_lingo/src/arches_lingo/components/schemes/SchemeBoxes.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { fetchSchemes } from "@/arches_lingo/api.ts";
+
+const schemes = await fetchSchemes();
+</script>
+
+<template>
+    <div style="display: block">
+        <!-- Creating New Scheme -->
+        <div>
+            <input
+                type="text"
+                placeholder="Enter Scheme Name"
+            />
+            <button>Create Scheme</button>
+        </div>
+
+        <!-- Existing Schemes -->
+        <div>
+            <ul>
+                <li
+                    v-for="scheme in schemes"
+                    :key="scheme.id"
+                >
+                    {{ scheme.name }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- arches_lingo/src/arches_lingo/pages/SchemeList.vue
+++ arches_lingo/src/arches_lingo/pages/SchemeList.vue
@@ -1,1 +1,13 @@
-<template>Schemes list placeholder</template>
+<script setup lang="ts">
+import ProgressSpinner from "primevue/progressspinner";
+import SchemeBoxes from "@/arches_lingo/components/schemes/SchemeBoxes.vue";
+</script>
+
+<template>
+    <Suspense>
+        <SchemeBoxes />
+        <template #fallback>
+            <ProgressSpinner />
+        </template>
+    </Suspense>
+</template>
