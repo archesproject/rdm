38dc632d4223b1b0a9a72ea8c3691aaa94126f2b
Jacob Walls <jwalls@fargeo.com>
2024-11-22 06:50:57 -0500

    Factor out <HierarchySplitter>

--- arches_lingo/src/arches_lingo/components/detail/ConceptDetail.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import type { Concept } from "@/arches_lingo/types";
-
-const { concept } = defineProps<{ concept: Concept }>();
-</script>
-
-<template>
-    <pre style="text-wrap: auto">{{ concept }}</pre>
-</template>
---
+++ arches_lingo/src/arches_lingo/pages/ConceptPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { inject } from "vue";
+
+import { displayedRowKey } from "@/arches_lingo/constants.ts";
+
+import type { DisplayedRowRefAndSetter } from "@/arches_lingo/types";
+
+const { displayedRow } = inject(
+    displayedRowKey,
+) as unknown as DisplayedRowRefAndSetter;
+</script>
+
+<template>
+    <pre style="text-wrap: auto">{{ displayedRow }}</pre>
+</template>
