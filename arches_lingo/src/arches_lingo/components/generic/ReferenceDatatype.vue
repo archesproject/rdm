0bb7b0361c09273f79e32cbfaef362108135bfbb
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-01-02 17:29:55 -0500

    pr feedback & readability improvements #147

--- arches_lingo/src/arches_lingo/components/generic/ReferenceDatatype.vue
+++ arches_lingo/src/arches_lingo/components/generic/ReferenceDatatype.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import { EDIT, VIEW } from "@/arches_lingo/constants.ts";
+import ReferenceDatatypeViewer from "@/arches_lingo/components/generic/reference-datatype/ReferenceDatatypeViewer.vue";
+import ReferenceDatatypeEditor from "@/arches_lingo/components/generic/reference-datatype/ReferenceDatatypeEditor.vue";
+
 import type {
     ControlledListItem,
     DataComponentMode,
 } from "@/arches_lingo/types";
-import ReferenceDatatypeViewer from "@/arches_lingo/components/generic/reference-datatype/ReferenceDatatypeViewer.vue";
-import ReferenceDatatypeEditor from "@/arches_lingo/components/generic/reference-datatype/ReferenceDatatypeEditor.vue";
-import { EDIT, VIEW } from "@/arches_lingo/constants.ts";
 
 defineProps<{
     mode?: DataComponentMode;
