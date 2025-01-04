ada257ce89b71558191a2b36410bc0feb57f0030
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-01-03 19:13:00 -0500

    Save / update scheme labels #147

--- arches_lingo/src/arches_lingo/components/generic/LabelEditor.vue
+++ arches_lingo/src/arches_lingo/components/generic/LabelEditor.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import { defineProps, inject, onMounted, ref, type Ref } from "vue";
+import { defineProps, inject, onMounted, ref, toRaw, type Ref } from "vue";
+import Button from "primevue/button";
+import { useGettext } from "vue3-gettext";
+import { useRoute } from "vue-router";
+import { useToast } from "primevue/usetoast";
+
 import {
     fetchControlledListOptions,
     fetchGroupRdmSystemList,
     fetchPersonRdmSystemList,
     fetchTextualWorkRdmSystemList,
+    updateSchemeLabel,
 } from "@/arches_lingo/api.ts";
 
 import NonLocalizedString from "@/arches_lingo/components/generic/NonLocalizedString.vue";
@@ -12,13 +18,14 @@ import ReferenceDatatype from "@/arches_lingo/components/generic/ReferenceDataty
 import ResourceInstanceRelationships from "@/arches_lingo/components/generic/ResourceInstanceRelationships.vue";
 
 import {
-    selectedLanguageKey,
     EDIT,
-    LANGUAGE_CONTROLLED_LIST,
-    LABEL_CONTROLLED_LIST,
-    STATUSES_CONTROLLED_LIST,
-    METATYPES_CONTROLLED_LIST,
+    ERROR,
     EVENT_TYPES_CONTROLLED_LIST,
+    LABEL_CONTROLLED_LIST,
+    LANGUAGE_CONTROLLED_LIST,
+    METATYPES_CONTROLLED_LIST,
+    selectedLanguageKey,
+    STATUSES_CONTROLLED_LIST,
 } from "@/arches_lingo/constants.ts";
 
 import type {
@@ -30,7 +37,12 @@ import type {
 } from "@/arches_lingo/types.ts";
 import type { Language } from "@/arches_vue_utils/types.ts";
 
+const emit = defineEmits(["update"]);
+const toast = useToast();
+const route = useRoute();
 const selectedLanguage = inject(selectedLanguageKey) as Ref<Language>;
+const { $gettext } = useGettext();
+
 const props = withDefaults(
     defineProps<{
         value?: AppellativeStatus;
@@ -49,8 +61,32 @@ const eventTypeOptions = ref<ControlledListItem[]>([]);
 const groupAndPersonOptions = ref<ResourceInstanceReference[]>();
 const textualWorkOptions = ref<ResourceInstanceReference[]>();
 
-function onUpdate(newValue: string) {
-    console.log(newValue);
+function onUpdate(
+    node: keyof AppellativeStatus,
+    val: ControlledListItem[] | ResourceInstanceReference[] | string,
+) {
+    if (Array.isArray(val)) {
+        (value.value[node] as unknown) = val.map((item) => toRaw(item));
+    } else {
+        (value.value[node] as unknown) = toRaw(val);
+    }
+}
+
+async function save() {
+    try {
+        await updateSchemeLabel(
+            route.params.id as string,
+            value.value.tileid as string,
+            value.value,
+        );
+        emit("update");
+    } catch (error) {
+        toast.add({
+            severity: ERROR,
+            summary: $gettext("Error saving scheme"),
+            detail: (error as Error).message,
+        });
+    }
 }
 
 async function getControlledListOptions(
@@ -117,7 +153,9 @@ onMounted(async () => {
     <NonLocalizedString
         :value="value?.appellative_status_ascribed_name_content ?? ''"
         :mode="EDIT"
-        @update="onUpdate"
+        @update="
+            (val) => onUpdate('appellative_status_ascribed_name_content', val)
+        "
     />
     <!-- Label Language: reference datatype -->
     <label for="">{{ $gettext("Label Language") }}</label>
@@ -126,7 +164,9 @@ onMounted(async () => {
         :mode="EDIT"
         :multi-value="false"
         :options="languageOptions"
-        @update="onUpdate"
+        @update="
+            (val) => onUpdate('appellative_status_ascribed_name_language', val)
+        "
     />
     <!-- Label Type: reference datatype -->
     <label for="">{{ $gettext("Label Type") }}</label>
@@ -135,7 +175,7 @@ onMounted(async () => {
         :mode="EDIT"
         :multi-value="false"
         :options="typeOptions"
-        @update="onUpdate"
+        @update="(val) => onUpdate('appellative_status_ascribed_relation', val)"
     />
     <!-- Label Status: reference datatype -->
     <label for="">{{ $gettext("Label Status") }}</label>
@@ -144,7 +184,7 @@ onMounted(async () => {
         :mode="EDIT"
         :multi-value="false"
         :options="statusOptions"
-        @update="onUpdate"
+        @update="(val) => onUpdate('appellative_status_status', val)"
     />
     <!-- Label Status Metatype: reference datatype -->
     <label for="">{{ $gettext("Label Metatype") }}</label>
@@ -153,7 +193,7 @@ onMounted(async () => {
         :mode="EDIT"
         :multi-value="false"
         :options="metatypeOptions"
-        @update="onUpdate"
+        @update="(val) => onUpdate('appellative_status_status_metatype', val)"
     />
     <!-- Label Temporal Validity Start: date -->
     <label for="">{{ $gettext("Label Temporal Validity Start") }}</label>
@@ -167,7 +207,9 @@ onMounted(async () => {
         :value="value?.appellative_status_data_assignment_actor"
         :mode="EDIT"
         :options="groupAndPersonOptions"
-        @update="onUpdate"
+        @update="
+            (val) => onUpdate('appellative_status_data_assignment_actor', val)
+        "
     />
     <!-- Sources: resource instance -->
     <label for="">{{ $gettext("Sources") }}</label>
@@ -175,7 +217,10 @@ onMounted(async () => {
         :value="value?.appellative_status_data_assignment_object_used"
         :mode="EDIT"
         :options="textualWorkOptions"
-        @update="onUpdate"
+        @update="
+            (val) =>
+                onUpdate('appellative_status_data_assignment_object_used', val)
+        "
     />
     <!-- Warrant Type: reference datatype -->
     <label for="">{{ $gettext("Warrant Type") }}</label>
@@ -184,6 +229,12 @@ onMounted(async () => {
         :mode="EDIT"
         :multi-value="false"
         :options="eventTypeOptions"
-        @update="onUpdate"
+        @update="
+            (val) => onUpdate('appellative_status_data_assignment_type', val)
+        "
     />
+    <Button
+        :label="$gettext('Update')"
+        @click="save"
+    ></Button>
 </template>
