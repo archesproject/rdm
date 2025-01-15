f71bacbb8118422dbd1166e1883154f122eb80f5
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-01-14 20:16:53 -0500

    Prevent multiple prefLabels in same language #147

--- arches_lingo/src/arches_lingo/api.ts
+++ arches_lingo/src/arches_lingo/api.ts
@@ -137,7 +137,10 @@ export const updateSchemeLabel = async (
         },
     );
     const parsed = await response.json();
-    if (!response.ok) throw new Error(parsed.message || response.statusText);
+    if (!response.ok)
+        throw new Error(
+            parsed.non_field_errors || parsed.message || response.statusText,
+        );
     return parsed;
 };
 
